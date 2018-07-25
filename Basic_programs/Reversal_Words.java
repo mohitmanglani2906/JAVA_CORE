fcf3037dc5dcc4b2b3c08510b69f3394af48c2f2
mohitmanglani2906 <38598962+mohitmanglani2906@users.noreply.github.com>
2018-07-25 14:02:04 +0530

    Update Reversal_Words.java

--- Basic_programs/Reversal_Words.java
+++ Basic_programs/Reversal_Words.java
@@ -4,7 +4,9 @@ class String_Demo
 	String str;
 	void show()
 		{
-			System.out.println(str);
+			System.out.print("Before reversal : ");
+			System.out.println(str+ "\ns");
+			System.out.print("After reversal : ");
 		}
 }
 class Reversal_Words
@@ -13,15 +15,17 @@ class Reversal_Words
 		{
 			Scanner s=new Scanner(System.in);
 			String_Demo d1=new String_Demo();
+			String s2=" ";
 			System.out.println("Enter your string ");
 			String s1=s.nextLine();
 			d1.str=s1;
+			String s3=s2+s1;
 			d1.show();
 			int cnt=0,j=0;
-				for(int i=s1.length()-1;i>=0;i--)
+				for(int i=s3.length()-1;i>=0;i--)
 					{
 						
-						char tch=s1.charAt(i);
+						char tch=s3.charAt(i);
 						if(tch!=' ')
 							{
 								cnt++;
@@ -31,7 +35,7 @@ class Reversal_Words
 						{
 							while(cnt>0)
 							{
-								char ch=s1.charAt(j++);
+								char ch=s3.charAt(j++);
 								System.out.print(ch);
 								cnt--;
 							}
