a2f7e16cbf2f692b8a7c1ce2dd077f9f5b23cfba
mohitmanglani2906 <38598962+mohitmanglani2906@users.noreply.github.com>
2018-08-23 18:04:14 +0530

    Committed

---
+++ Exception/Throws_Keyword.java
@@ -0,0 +1,30 @@
+// Here we will see throws keyword i.e we can use it in method defunition
+class Demo
+{
+	public void show() throws ClassNotFoundException,ArrayIndexOutOfBoundsException //more than one exception canbe thrown
+	{
+		System.out.println("In show method");
+		throw new ClassNotFoundException();   // here this exception will be thrown
+	}
+}
+class Throws_Keyword 
+{
+	public static void main(String[] args) 
+	{
+		Demo d1=new Demo();
+		int a[]=new int[10];
+		try
+		{
+			d1.show();
+			a[11]=56;
+		}
+		catch(ClassNotFoundException ce) // here this exception will be caught as we are throwing it from class
+		{
+			System.out.println(ce);
+		}
+		catch(ArrayIndexOutOfBoundsException ae)
+		{
+			System.out.println(ae);
+		}
+	}
+}
